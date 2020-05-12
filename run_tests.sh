4c5606d1fa24d5fab6bc1ceb6ae4c7a60c497bb0
Chun-Min Chang <chun.m.chang@gmail.com>
2020-05-12 11:50:18 -0700

    Run aggregate tests only

--- run_tests.sh
+++ run_tests.sh
@@ -14,39 +14,45 @@ echo "RUST_BACKTRACE is set to ${RUST_BACKTRACE}\n"
 # between this crate and the <SUB_CRATE> can be saved. The compiling for <SUB_CRATE> can be reused
 # when running `cargo *` with this crate.
 # -------------------------------------------------------------------------------------------------
-SUB_CRATE="coreaudio-sys-utils"
+# SUB_CRATE="coreaudio-sys-utils"
 
 # Format check
 # `cargo fmt -p *` is only usable in workspaces, so a workaround is to enter to the sub crate
 # and then exit from it.
-cd $SUB_CRATE
-cargo fmt --all -- --check
-cd ..
+# cd $SUB_CRATE
+# cargo fmt --all -- --check
+# cd ..
 
 # Lints check
-cargo clippy -p $SUB_CRATE -- -D warnings
+# cargo clippy -p $SUB_CRATE -- -D warnings
 
 # Regular Tests
-cargo test -p $SUB_CRATE
+# cargo test -p $SUB_CRATE
 
 # Run tests in the main crate
 # -------------------------------------------------------------------------------------------------
 # Format check
-cargo fmt --all -- --check
+# cargo fmt --all -- --check
 
 # Lints check
-cargo clippy -- -D warnings
+# cargo clippy -- -D warnings
 
 # Regular Tests
-cargo test --verbose
-cargo test test_configure_output -- --ignored
-cargo test test_aggregate -- --ignored --test-threads=1
+# cargo test --verbose
+# cargo test test_configure_output -- --ignored
+round=1
+while [ $round -le 100 ]
+do
+    echo "Test round $round\n"
+    cargo test test_aggregate -- --ignored --test-threads=1
+    round=$(( $round + 1 ))
+done
 
 # Parallel Tests
-cargo test test_parallel -- --ignored --nocapture --test-threads=1
+# cargo test test_parallel -- --ignored --nocapture --test-threads=1
 
 # Device-changed Tests
-sh run_device_tests.sh
+# sh run_device_tests.sh
 
 # Manual Tests
 # cargo test test_switch_output_device -- --ignored --nocapture
